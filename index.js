a11acee401eead984b509d334b2086b3022d8d74
Aleksey Kozachenko <kozachenko.aleksey@gmail.com>
2014-12-24 17:20:09 +0200

    INIT

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var compression = require('compression');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(compression());
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
