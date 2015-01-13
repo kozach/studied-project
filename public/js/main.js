6648015d9fc12a9f9853c44f9d9026ea45976c93
Aleksey Kozachenko <kozachenko.aleksey@gmail.com>
2015-01-13 21:06:26 +0200

    Test render

--- public/js/main.js
+++ public/js/main.js
@@ -7,12 +7,24 @@
   // });
   // var Source = Backbone.Model.extend({
   //   defaults: {
-  //     author: ""
+  //     source: "",
+  //     format: "",
+  //     subject: "",
+  //     title: "",
+  //     titleOriginal: "",
+  //     rating: "",
+  //     duration: "",
+  //     year: "",
+  //     description: "",
+  //     review: "",
+  //     link: "",
+  //     language: ""
   //   },
-  //   urlRoot: "/api/sources"
+  //   urlRoot: "/api/sources",
+  //   idAttribute: "_id"
   // });
   // var SourcesListView = Backbone.View.extend({
-  //   el: '.page',
+  //   el: '#main',
   //   render: function() {
   //     var sources = new Sources();
   //     var that = this;
@@ -30,7 +42,7 @@
   // var sourcesListView = new SourcesListView();
 
   // var SourcesEditView = Backbone.View.extend({
-  //   el: '.page',
+  //   el: '#main',
   //   events: {
   //     'submit .sources-edit-form': 'saveItem',
   //     'click .delete': 'deleteItem',
@@ -87,6 +99,7 @@
   // });
 
   // var sourcesEditView = new SourcesEditView();
+
   // var Router = Backbone.Router.extend({
   //   routes: {
   //     "": "home",
@@ -106,26 +119,27 @@
   // })
   // Backbone.history.start();
 
+
   var Contact = Backbone.Model.extend({
     urlRoot: '/api/sources',
-    // defaults: {
-    //   startDate: [],
-    //   endDate: [],
-    //   source: '',
-    //   format: '',
-    //   subject: '',
-    //   title: '',
-    //   author: [],
-    //   titleOriginal: '',
-    //   rating: '',
-    //   duration: '',
-    //   year: '',
-    //   description: '',
-    //   review: '',
-    //   link: '',
-    //   language: '',
-    //   tags: []
-    // }
+  // defaults: {
+  // startDate: [],
+  // endDate: [],
+  // source: '',
+  // format: '',
+  // subject: '',
+  // title: '',
+  // author: [],
+  // titleOriginal: '',
+  // rating: '',
+  // duration: '',
+  // year: '',
+  // description: '',
+  // review: '',
+  // link: '',
+  // language: '',
+  // tags: []
+  // }
     idAttribute: "_id",
   });
 
@@ -149,11 +163,7 @@
     initialize: function() {
       var that = this;
       that.collection = new Directory();
-      that.collection.fetch({
-        success: function() {
-          that.render();
-        }
-      });
+      that.collection.fetch({async:false});
     },
     render: function() {
       var that = this;
@@ -165,35 +175,49 @@
       var contactView = new ContactView({
         model: item
       });
-      // item.save();
       this.$el.append(contactView.render().el);
     }
   });
 
   var directory = new DirectoryView();
 
-// _.each(jsonD, function(item) {
-//   item.tags = item.tags.split(",");
-//   item.author = item.author.split(",");
-//   item.endDate = item.endDate.split(",");
-//   item.startDate = item.startDate.split(",");
-//   _.each(item.tags, function(val, key) {
-//     item.tags[key] = val.trim();
-//   });
-//   _.each(item.author, function(val, key) {
-//     item.author[key] = val.trim();
-//   });
-//   _.each(item.endDate, function(val, key) {
-//     item.endDate[key] = val.trim();
-//   });
-//   _.each(item.startDate, function(val, key) {
-//     item.startDate[key] = val.trim();
-//   });
-//   delete item.source
-//   delete item.subject
-//   delete item.description
-//   delete item.language
-// });
-// console.log(JSON.stringify(jsonD));
+  var Router = Backbone.Router.extend({
+      routes: {
+        "": "home",
+        "source/:id": "edit",
+        "source/add": "add",
+      }
+  });
+
+  var router = new Router;
+  router.on('route:home', function() {
+    directory.render();
+  })
+  Backbone.history.start();
+
+
+  // _.each(jsonD, function(item) {
+  //   item.tags = item.tags.split(",");
+  //   item.author = item.author.split(",");
+  //   item.endDate = item.endDate.split(",");
+  //   item.startDate = item.startDate.split(",");
+  //   _.each(item.tags, function(val, key) {
+  //     item.tags[key] = val.trim();
+  //   });
+  //   _.each(item.author, function(val, key) {
+  //     item.author[key] = val.trim();
+  //   });
+  //   _.each(item.endDate, function(val, key) {
+  //     item.endDate[key] = val.trim();
+  //   });
+  //   _.each(item.startDate, function(val, key) {
+  //     item.startDate[key] = val.trim();
+  //   });
+  //   delete item.source
+  //   delete item.subject
+  //   delete item.description
+  //   delete item.language
+  // });
+  // console.log(JSON.stringify(jsonD));
 
 })(jQuery);
